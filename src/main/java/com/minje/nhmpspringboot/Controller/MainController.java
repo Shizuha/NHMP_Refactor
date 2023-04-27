cf869b0611ccc21b22a6787f667b7838b788b813
Shizuha <rlaghrb123@naver.com>
2023-04-27 20:31:54 +0900

    MainController 수정 및 application 내용 수정

--- src/main/java/com/minje/nhmpspringboot/Controller/MainController.java
+++ src/main/java/com/minje/nhmpspringboot/Controller/MainController.java
@@ -1,13 +1,16 @@
 package com.minje.nhmpspringboot.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.*;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String functionIntro() {
-        return "index"; // 커밋 테스트
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    @RequestMapping(path = "examples/landing") // 이곳으로 들어오는 API주소를 mapping, examples/landing주소로 받겠다 (localhost:8080/examples/landing)
+    public String functionIntro() { 
+        log.info("이 페이지로 잘 들어갔나요?");
+        return "landing";
     }
 }
